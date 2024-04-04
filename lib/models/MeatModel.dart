504a54af1e7759a091c75d4b0f218ea3cb3bf164
Fazil <fazilnazkolambil@gmail.com>
2024-04-04 14:28:53 +0530

    corrections done

--- lib/models/MeatModel.dart
+++ lib/models/MeatModel.dart
@@ -1,11 +1,12 @@
 class MeatModel{
-  String? image, category, name, ingredients, rate, quantity, description, id;
-  MeatModel({ this.image, this.category, this.name, this.ingredients, this.rate, this.quantity, this.description, this.id});
+  String? image, category, type, name, ingredients, rate, quantity, description, id;
+  MeatModel({ this.image, this.category,this.type, this.name, this.ingredients, this.rate, this.quantity, this.description, this.id});
 
   Map <String, dynamic> toMap(){
     return {
       "Image" : this.image,
       "category" : this.category,
+      "type" : this.type,
       "name" : this.name,
       "ingredients" : this.ingredients,
       "rate" : this.rate,
@@ -18,6 +19,7 @@ factory MeatModel.fromMap(Map <String, dynamic> map){
     return MeatModel(
       image: map["image"] ?? "",
       category: map["category"] ?? "",
+      type: map["type"] ?? "",
       name: map["name"] ?? "",
       ingredients: map["ingredients"] ?? "",
       rate: map["rate"] ?? "",
@@ -27,11 +29,12 @@ factory MeatModel.fromMap(Map <String, dynamic> map){
     );
 }
 MeatModel copyWith({
-    String? image, category, name, ingredients, rate, quantity, description, id
+    String? image, category, type, name, ingredients, rate, quantity, description, id
 }){
     return MeatModel(
       image: image ?? this.image,
       category: category ?? this.category,
+      type: type ?? this.type,
       name: name ?? this.name,
       ingredients: ingredients ?? this.ingredients,
       rate: rate ?? this.rate,
