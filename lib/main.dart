221842cfdb3a62662568480d98b152d2130d62ed
DELL <binsiyanazer21@gmail.com>
2024-05-14 11:32:48 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:meat_admin/features/addingPages/AddMeats.dart';
 import 'package:meat_admin/features/listPages/MeatTypeList.dart';
@@ -16,7 +17,7 @@ Future <void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child:  MyApp()));
 }
 MeatModel? currentMeatModel;
  var scrWidth;
